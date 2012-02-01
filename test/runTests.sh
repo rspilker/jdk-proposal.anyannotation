b84c84c1e8b60f2016b866fa760e3b00fc88a8d1
Reinier Zwitserloot <reinier@zwitserloot.com>
2012-02-02 00:06:18 +0100

    Attempt to make test script run on crappy bash versions.

--- test/runTests.sh
+++ test/runTests.sh
@@ -16,7 +16,7 @@ do
 			FAILED=$[FAILED+1]
 		else
 			java -javaagent:../dist/anyannotation.jar -cp . reflection.`basename $BASENAME` &>"$BASENAME.result.actual"
-			diff -qaiBwN --strip-trailing-cr "$BASENAME.result.actual" "$BASENAME.result.txt" >/dev/null
+			diff -qaiBwN "$BASENAME.result.actual" "$BASENAME.result.txt" >/dev/null
 			if [ $? == 0 ]; then
 				echo " OK"
 				PASSED=$[PASSED+1]
@@ -36,7 +36,7 @@ do
 			cat "$BASENAME.compiler-result.actual"
 			FAILED=$[FAILED+1]
 		else
-			diff -qaiBwN --strip-trailing-cr "$BASENAME.compiler-result.actual" "$BASENAME.compiler-result.txt" >/dev/null
+			diff -qaiBwN "$BASENAME.compiler-result.actual" "$BASENAME.compiler-result.txt" >/dev/null
 			if [ $? != 0 ]; then
 				echo " FAIL: Compiler errors appropriately but error message is incorrect."
 				echo "Expected error:"
